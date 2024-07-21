3509687364813d01bedff62f950fff651c805f62
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-07-21 16:33:30 +0530

    META-INF

---
+++ StudentManagementProtal/src/main/java/lk/ijse/gdse68/studentmanagementprotal/filter/Security.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse68.studentmanagementprotal.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebFilter(urlPatterns = "/*")
+public class Security  extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Security Filter");
+        chain.doFilter(req, res);
+    }
+}
